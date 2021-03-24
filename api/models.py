e396a09243dc4c4e54a1d4de806b6e6414e43d39
suy2on <tnwjdrms@naver.com>
2021-03-24 23:24:04 +0900

    Create Post and Photo model -setting for media file -setting for auth -Post model is one to many relation each with Photo and User

--- api/models.py
+++ api/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+import datetime
 
+# User 와 1대1 관계
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nickname = models.TextField(max_length=10)
+    nickname = models.CharField(max_length=10)
     comment = models.TextField(max_length= 200)
 
+# User 와 1대다 관계
+class Post(models.Model):
+    written = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL) # 로그인된 user
+    pub_date = models.DateTimeField('date published', default=datetime.datetime.now, editable=False)
+
+class Photo(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="image")  # media/image에 이미지들 저
 
 
--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import json
+from django.conf import settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -117,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# auth
+AUTH_USER_MODEL = 'api.User'
+
+# media upload
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
