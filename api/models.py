fc51f275a868bc222881769ca64a304aca9f5bfe
suy2on <tnwjdrms@naver.com>
2021-03-24 22:11:21 +0900

    Create Profile model Profile is one to one relation with User model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.TextField(max_length=10)
+    comment = models.TextField(max_length= 200)
+
+
+
