ae66fa3b93c9136d541d6d4190f6a760cb7af43c
suy2on <tnwjdrms@naver.com>
2021-04-08 21:29:09 +0900

    Add get, put function in views.py

--- api/views.py
+++ api/views.py
@@ -5,8 +5,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+
 class PostList(APIView):
-    def get(self, request, format=None,):
+    def get(self, request, format=None, ):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
@@ -20,9 +21,18 @@ class PostList(APIView):
 
 
 class PostDetail(APIView):
-		def get(self, request, pk, format=None):
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
 
-        def update(sefl, request, pk, format=None):
 
+    def put(self, request, pk, format=None):
+        post = self.get_object(pk =pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-		def delete(self, request, pk, format=None):
+    def delete(self, request, pk, format=None):
