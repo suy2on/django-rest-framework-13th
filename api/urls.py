5bbeae305eb9e58862dc9746435b307acff80d55
suy2on <tnwjdrms@naver.com>
2021-05-16 01:28:19 +0900

    Modify api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from api import views
 
 from rest_framework import routers
-from .views import PostViewSet
+from .views import PostViewSet, ProfileViewSet
 
 router = routers.DefaultRouter()
 router.register(r'contents', PostViewSet)   # register()함으로써 두 개의 url 생성
+router.register(r'users', ProfileViewSet)
 
 urlpatterns = router.urls
 
